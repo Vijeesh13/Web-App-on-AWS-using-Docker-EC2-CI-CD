c7955cc707c67e6733f97541b45d12b52a8c3146
Vijeesh13 <vijeesh.1398@gmail.com>
2025-12-03 19:30:16 +0530

    Update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from DevOps Project!");
+  res.end("Hello!! From DevOps Project!");
 });
 
 server.listen(port, () => {
